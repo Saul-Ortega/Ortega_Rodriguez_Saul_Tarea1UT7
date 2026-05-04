8c37caaeef4c14b5dee0fbdf7e6ff2fdf0533961
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2026-05-04 11:26:15 +0100

    feat: Search by author through speech recognition

--- src/components/voz/VozLibrosComandos.jsx
+++ src/components/voz/VozLibrosComandos.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import searchByCategory from '../../controller/searchByCategory'
 import BookCard from '../BookCard'
+import searchByAuthor from '../../controller/searchByAuthor'
 
 export default function VozLibrosComandos() {
     const [message, setMessage] = useState('')
@@ -12,6 +13,18 @@ export default function VozLibrosComandos() {
                 const result = await searchByCategory(category);
                 const booksList = result?.books || [];
                 setMessage(booksList.map((book, i) => <BookCard key={book.id || book.title || i} book={book} />))
+            }
+        },
+        {
+            command: 'busca por autor *',
+            callback: async (author) => {
+                const result = await searchByAuthor(author);
+                const authorsList = result?.authors || [];
+                setMessage(
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: 20 }}>
+                        {authorsList.map((author, i) => <span key={author.id || i} style={{ marginTop: 10 }}>{author.name || "Sin nombre"}</span>)}
+                    </div>
+                )
             }
         },
         {
