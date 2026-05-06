2746baa9b8e55582849f75c15349162ee1ea0174
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2026-05-06 15:40:17 +0100

    style: Improve rating format

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -5,7 +5,7 @@ export default function BookCard({book}) {
                 <div style={{marginTop: 10}}>Título: {book?.title || "Sin Título"}</div>
                 <img src={book?.image || ""} alt={book?.title || "No hay imagen"} style={{ width: 180, marginTop: 10  }} />
                 <div style={{ marginTop: 10 }}>Autor/es: {book?.authors?.map((author) => <span key={author.id || author.name}>{author.name} </span>) || "Sin autor/es"}</div>
-                <div style={{ marginTop: 10 }}>Puntuación: {Math.round(book?.rating?.average) || "No hay puntuación"}</div>
+                <div style={{ marginTop: 10 }}>Puntuación: {Math.round(book?.rating?.average * 100) + "%" || "No hay puntuación"}</div>
             </div>
         </>
     );
